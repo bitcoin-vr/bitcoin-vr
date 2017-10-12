19da8d0a823d878e98d1f050323cc76e89fc8d84
Robin Wilson <robin.kath.wilson@gmail.com>
2017-10-12 11:22:41 -0700

    adding rotation to zeppelin

--- src/components/common/Zeppelin.js
+++ src/components/common/Zeppelin.js
@@ -14,7 +14,10 @@ class Zeppelin extends React.Component {
     super(props);
     this.state = {
       y: new Animated.Value(-10),
-      readoutVisible: false
+      // x: new Animated.Value(props.x),
+      // xMoveTo: props.x +
+      // readoutVisible: false
+      rotation: Math.floor(Math.random()*360)
     }
     this.animate = this.animate.bind(this);
     this.toggleReadoutVisible = this.toggleReadoutVisible.bind(this);
@@ -34,71 +37,83 @@ class Zeppelin extends React.Component {
     ).start()
   }
 
-  toggleReadoutVisible() {
-    this.state.readoutVisible
-      ? this.setState({ readoutVisible: false })
-      : this.setState({ readoutVisible: true })
-  }
+//   animateX() {
+//     Animated.timing(
+//       this.state.y,
+//       {
+//         toValue: 100,
+//         duration: 100000
+//       }
+//     ).start()
+//   }
+// }
 
-  render() {
-    const { x, z, volume, color, scale } = this.props.transaction;
-    const base = 5;
+toggleReadoutVisible() {
+  this.state.readoutVisible
+    ? this.setState({ readoutVisible: false })
+    : this.setState({ readoutVisible: true })
+}
 
-    return (
-      <Animated.View
-        onEnter={() => this.toggleReadoutVisible()}
-        onExit={() => this.toggleReadoutVisible()}
-        style={{
-          transform: [
-            { translate: [x, 0, z] },
-            { translateY: this.state.y }
-          ]
-        }}>
-        {
-              <Model
-                lit
-                texture={asset('gold_texture.jpg')}
-                source={{
-                  obj: asset('Low-Poly_airship.obj'),
-                }}
-                color={color}
-                style={{
-                  transform: [
-                    { scale: 0.1 },
-                    { translate: [0, 100, 0] }
-                  ]
-                }}
-              >
-                <Text>
-                  {volume}
-                </Text>
-              </Model>
+render() {
+  const { x, z, volume, color, scale } = this.props.transaction;
+  const base = 5;
 
-        }
-
-        <Box
+  return (
+    <Animated.View
+      onEnter={() => this.toggleReadoutVisible()}
+      onExit={() => this.toggleReadoutVisible()}
+      style={{
+        transform: [
+          { translate: [x, 0, z] },
+          { translateY: this.state.y },
+          { rotateY: this.state.rotation}
+        ]
+      }}>
+      {
+        <Model
           lit
-          dimWidth={base * scale || 30}
-          dimDepth={base * scale || 30}
-          dimHeight={base * scale || 30}
+          texture={asset('gold_texture.jpg')}
+          source={{
+            obj: asset('Low-Poly_airship.obj'),
+          }}
+          color={color}
           style={{
             transform: [
-              { translate: [0, -base * scale / 2 + 1, 0] }
-            ],
-            color: color
+              { scale: 0.1 },
+              { translate: [0, 100, 0] }
+            ]
           }}
-        />
-        {
-          this.state.readoutVisible && volume && <DataReadout readout={{
-            boxSize: base * scale || 30,
-            x,
-            y: this.state.y,
-            z,
-            orig: this.props.transaction
-          }} />
-        }
-      </Animated.View>)
-  }
+        >
+          <Text>
+            {volume}
+          </Text>
+        </Model>
+
+      }
+
+      <Box
+        lit
+        dimWidth={base * scale || 30}
+        dimDepth={base * scale || 30}
+        dimHeight={base * scale || 30}
+        style={{
+          transform: [
+            { translate: [0, -base * scale / 2 + 1, 0] }
+          ],
+          color: color
+        }}
+      />
+      {
+        this.state.readoutVisible && volume && <DataReadout readout={{
+          boxSize: base * scale || 30,
+          x,
+          y: this.state.y,
+          z,
+          orig: this.props.transaction
+        }} />
+      }
+    </Animated.View>)
+}
 }
 
 export { Zeppelin };
