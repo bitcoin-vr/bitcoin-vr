1272a85dde52bf810d57fad0af3b821d901bf972
Daniel Onggunhao <danielo@cs.stanford.edu>
2017-10-13 17:51:29 +0700

    Prettify dataReadout box

--- src/components/common/DataReadout.js
+++ src/components/common/DataReadout.js
@@ -3,8 +3,13 @@ import {
   Box,
   Animated,
   Text,
-  View
+  View,
+  StyleSheet
 } from 'react-vr';
+
+// Layout props
+const _panelHeight = 30
+const _panelWidth = 40;
 
 class DataReadout extends React.Component {
   constructor(props) {
@@ -14,36 +19,84 @@ class DataReadout extends React.Component {
   render() {
     const { vrX, vrY, vrZ, orig, boxSize } = this.props.readout;
     const { price, volume, exchange_id, market_id } = orig;
-    console.log(typeof price, typeof volume);
 
+    // Calculate USD value
+    // TODO: refactor this to the Redux store (instead of calculating during every re-render)
     let usd = (volume * price);
     usd = usd.toFixed(2);
 
     return (
       <View
         billboarding={'on'}
-
+        style={styles.container}
       >
         <Text
-          style={{
-            backgroundColor: '#00000080',
-            fontSize: 1,
-            color: 'white',
-            position: 'absolute',
-            width: 10,
-            transform: [
-              { translate: [-10, - boxSize - 3, boxSize] },
-              { scale: 4 }
-            ],
-          }}
+          style={styles.transactionVolume}
         >
           {
-            `BTC: ${volume.toFixed(2)}\nUSD Value: ${usd}\nExchange: ${market_id}`
+            `${volume.toFixed(1)}\n`
+          }
+        </Text>
+        <Text
+          style={styles.transactionValue}
+        >
+          {
+            `USD ${usd}\n`
+          }
+        </Text>
+        <Text
+          style={styles.transactionExchange}
+        >
+          {
+            `${capitalizeFirstLetter(exchange_id)}`
           }
         </Text>
       </View>
     )
   }
 }
+
+// Helper for capitalization
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#00000080',
+    position: 'absolute',
+    height: _panelHeight,
+    width: _panelWidth,
+    flexDirection: 'column',
+    transform: [
+      { translate: [-_panelWidth / 2, -_panelHeight / 10, 0] },
+      { scale: 4 }
+    ],
+    borderRadius: 1,
+    justifyContent: 'flex-start',
+  },
+  transactionVolume: {
+    flex: 2,
+    fontSize: 10,
+    color: 'white',
+    width: _panelWidth,
+    textAlign: 'center',
+    fontWeight: '400'
+  },
+  transactionValue: {
+    flex: 1,
+    fontSize: 4,
+    color: 'white',
+    width: _panelWidth,
+    textAlign: 'center'
+  },
+  transactionExchange: {
+    flex: 1,
+    fontSize: 4,
+    color: 'white',
+    width: _panelWidth,
+    textAlign: 'center'
+  }
+})
 
 export { DataReadout };
