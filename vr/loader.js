2d5a5b0a673e83c0a3a1476efdd696cc02cc831c
Robin Wilson <robin.kath.wilson@gmail.com>
2017-11-06 15:08:35 -0800

    loader fades and is removed from DOM on React-VR load

--- vr/loader.js
+++ vr/loader.js
@@ -9,15 +9,16 @@ const removeLoader =  () => {
     var loaderWrapper = document.getElementById("loader-wrapper");
     console.log('Added FADE!');
 
-    loaderId.className += "fade";
+    loaderWrapper.className += "fade-out";
     setTimeout(() => {
-      console.log('Hello blockchain!');
-    }, 5000);
+      var garbage = loaderId.removeChild(loaderWrapper);
+    }, 2000);
 
     //loaderId.addEventListener("fade", , false);
+    //prefixedEvent(heart, "AnimationIteration", AnimationListener);
 
 
-    //var garbage = loaderId.removeChild(loaderWrapper);
+
 
     // var element = document.getElementById("loader-wrapper");
     // if(element) {
@@ -28,6 +29,25 @@ const removeLoader =  () => {
     // }
   };
 
+//   function AnimationListener() {
+//     if(hovered)
+//     {
+//       heart.classList.remove('animated');
+//       heart.style.webkitTransform = 'scale(2)';
+//       heart.style.MozTransform = 'scale(2)';
+//       heart.style.msTransform = 'scale(2)';
+//       heart.style.OTransform = 'scale(2)';
+//       heart.style.transform = 'scale(2)';
+//     }
+// }
+
+
+//   function prefixedEvent(element, type, callback) {
+//     for (var p = 0; p < pfx.length; p++) {
+//         if (!pfx[p]) type = type.toLowerCase();
+//         element.addEventListener(pfx[p]+type, callback, false);
+//     }
+// }
 // $(document).ready(function() {
 
 // 	setTimeout(function(){
