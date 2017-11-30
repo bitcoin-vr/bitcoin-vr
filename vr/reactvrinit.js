fa9f1d200d014d8c11635e647362ab34eaa14f31
Robin Wilson <robin.kath.wilson@gmail.com>
2017-11-30 10:53:46 -0800

    combined remove warning ribbon if mobile into removeloader function

--- vr/reactvrinit.js
+++ vr/reactvrinit.js
@@ -1,12 +1,5 @@
 ReactVR.removeLoader();
 
-// remove warning ribbon if viewing on mobile
-if (document.mobile) {
-  var ribbonWrapper = document.getElementById('ribbon-wrapper');
-  var ribbon = document.getElementById('ribbon');
-  ribbonWrapper.removeChild(ribbon);
-} // Firefox Nightly change #ribbon content to "Browser is compatible to use VR headset. Best experience in VR headset."
-
 // Initialize the React VR application
 ReactVR.init(
   // When you're ready to deploy your app, update this line to point to
